f70659593f65b61c39e31604ee04dd8d163f1e61
Marina Zhukova <marinazhukova28@gmail.com>
2024-03-29 20:43:11 +0200

    add empty pj

---
+++ Lesson11HW/Lesson11HW/Source/Screen/ContactUsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ContactUsViewController.swift
+//  Lesson11HW
+//
+
+//
+
+import UIKit
+
+class ContactUsViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Lesson11HW/Lesson11HW/Source/Screen/TermsPrivacyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TermsPrivacyViewController.swift
+//  Lesson11HW
+//
+
+//
+
+import UIKit
+
+class TermsPrivacyViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
